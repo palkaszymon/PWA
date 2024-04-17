13317299ca586b3b35ab9097b9f5ee24692ff9de
s-121-30 <s-121-30@gumiforest.ae.krakow.pl>
2024-04-17 14:44:55 +0200

    sos change

--- js/sos.js
+++
@@ -1,9 +0,0 @@
-function sendSOS() {
-    console.log('vibrate')
-    if ("vibrate" in navigator) {
-        navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]);
-    }
-    else {
-        alert("Vibration not supported by your browser")
-    }
-}
